99d0f48035d05ea8e851a2cf4f8b78ab65ca72a1
Shafinaz <shafinazyasin@gmail.com>
2024-08-25 14:27:30 -0400

    base case

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
+#include <string>
+
 int main()
 {
+    std::string input = "";
+
+   /* while(input.compare("exit"))
+    {
+        std::cout << "$";
+        std::cin >> input;
+    }*/
+
     return 0;
 }
